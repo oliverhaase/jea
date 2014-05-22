911dbe8b104229e3e9fc6fc82d7145d6ab7743d1
Oliver Haase <haase@htwg-konstanz.de>
2014-05-22 13:26:18 +0200

    Comment added.

--- src/main/java/de/htwg_konstanz/jea/vm/Frames.java
+++ src/main/java/de/htwg_konstanz/jea/vm/Frames.java
@@ -4,6 +4,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Frames {
+	// Creates new set of processed frames. If the processed frames are copies
+	// of the original frames depends on the frame processor.
 	public static Set<Frame> processFrames(Set<Frame> originals, FrameProcessor processor) {
 		Set<Frame> resultSet = new HashSet<Frame>();
 		for (Frame original : originals)
