2c9ba5ec50bbbdcd67ff9f89e0107496cfeb813f
Oliver Haase <haase@htwg-konstanz.de>
2014-05-26 14:43:38 +0200

    Field origin added; only used for sub-objects.

--- src/main/java/de/htwg_konstanz/jea/vm/ObjectNode.java
+++ src/main/java/de/htwg_konstanz/jea/vm/ObjectNode.java
@@ -8,7 +8,7 @@ import net.jcip.annotations.Immutable;
 @EqualsAndHashCode(exclude = { "escapeState" })
 public class ObjectNode implements Node {
 	private final static ObjectNode global = new ObjectNode("global", EscapeState.GLOBAL_ESCAPE,
-			null, false);
+			null, false, null);
 
 	@Getter
 	private final String id;
@@ -18,24 +18,28 @@ public class ObjectNode implements Node {
 	private final String type;
 	@Getter
 	private final boolean isPhantom;
+	@Getter
+	private final ObjectNode origin;
 
-	private ObjectNode(String id, EscapeState escapeState, String type, boolean isPhantom) {
+	private ObjectNode(String id, EscapeState escapeState, String type, boolean isPhantom,
+			ObjectNode origin) {
 		this.id = id;
 		this.escapeState = escapeState;
 		this.type = type;
 		this.isPhantom = isPhantom;
+		this.origin = origin;
 	}
 
 	public static ObjectNode newPhantomObjectNode(int id) {
-		return new ObjectNode("p" + id, EscapeState.ARG_ESCAPE, null, true);
+		return new ObjectNode("p" + id, EscapeState.ARG_ESCAPE, null, true, null);
 	}
 
 	public static ObjectNode newInternalObjectNode(String id, String type) {
-		return new ObjectNode(id, EscapeState.NO_ESCAPE, type, false);
+		return new ObjectNode(id, EscapeState.NO_ESCAPE, type, false, null);
 	}
 
 	public static ObjectNode newSubObjectNode(ObjectNode origin, String fieldName) {
-		return new ObjectNode(origin.id + "." + fieldName, origin.escapeState, null, true);
+		return new ObjectNode(origin.id + "." + fieldName, origin.escapeState, null, true, origin);
 	}
 
 	public static ObjectNode getGlobalObjectNode() {
@@ -44,7 +48,7 @@ public class ObjectNode implements Node {
 
 	public ObjectNode increaseEscapeState(EscapeState escapeState) {
 		if (this.escapeState.moreConfinedThan(escapeState))
-			return new ObjectNode(this.getId(), escapeState, this.type, this.isPhantom);
+			return new ObjectNode(this.getId(), escapeState, this.type, this.isPhantom, this.origin);
 		return this;
 	}
 
