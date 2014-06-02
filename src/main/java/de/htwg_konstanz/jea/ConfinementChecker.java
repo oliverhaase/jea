3a330ac58e5c5ee17ac834b172404a81c8bc546b
Oliver Haase <haase@htwg-konstanz.de>
2014-06-02 13:24:50 +0200

    Classes to be checked adapted.

--- src/main/java/de/htwg_konstanz/jea/ConfinementChecker.java
+++ src/main/java/de/htwg_konstanz/jea/ConfinementChecker.java
@@ -5,12 +5,11 @@ import de.htwg_konstanz.jea.gen.Program;
 public class ConfinementChecker {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		String[] classes = { // "de.htwg_konstanz.jea.AstConverter",
-		"de.htwg_konstanz.jea.AstConverterVisitor", "de.htwg_konstanz.jea.ConfinementChecker",
-				"de.htwg_konstanz.jea.CreationChecker", "de.htwg_konstanz.jea.ProgramBuilder",
-				"de.htwg_konstanz.jea.vm.InternalObject", "de.htwg_konstanz.jea.TccTestClass",
-		// "de.seerhein_lab.jic.AnalysisResult",
-		// "de.seerhein_lab.jic.EmercencyBrakeException"
+		String[] classes = { "de.htwg_konstanz.jea.AstConverterVisitor",
+				"de.htwg_konstanz.jea.ConfinementChecker", "de.htwg_konstanz.jea.CreationChecker",
+				"de.htwg_konstanz.jea.ProgramBuilder", "de.htwg_konstanz.jea.TccTestClass",
+				"de.htwg_konstanz.jea.vm.ConnectionGraph",
+		// "de.htwg_konstanz.jea.vm.DontCareSlot"
 		};
 
 		Program program = new ProgramBuilder(classes).build();
