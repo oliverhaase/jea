bb3fdfacaa9e518a3e33b4f3db0f5f3104e060e6
Oliver Haase <haase@htwg-konstanz.de>
2014-06-04 15:27:05 +0200

    All jea classes added, escaping classes listed in the result output.

--- src/main/java/de/htwg_konstanz/jea/ConfinementChecker.java
+++ src/main/java/de/htwg_konstanz/jea/ConfinementChecker.java
@@ -5,15 +5,42 @@ import de.htwg_konstanz.jea.gen.Program;
 public class ConfinementChecker {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		String[] classes = { "de.htwg_konstanz.jea.AstConverterVisitor",
-				"de.htwg_konstanz.jea.ConfinementChecker", "de.htwg_konstanz.jea.CreationChecker",
-				"de.htwg_konstanz.jea.ProgramBuilder", "de.htwg_konstanz.jea.TccTestClass",
-				"de.htwg_konstanz.jea.vm.ConnectionGraph", "de.htwg_konstanz.jea.vm.Slot",
-				"de.htwg_konstanz.jea.vm.DontCareSlot" };
+		String[] classes = {
+				// "de.htwg_konstanz.jea.AstConverter",
+				"de.htwg_konstanz.jea.AstConverterVisitor",
+				"de.htwg_konstanz.jea.ConfinementChecker",
+				"de.htwg_konstanz.jea.CreationChecker",
+				"de.htwg_konstanz.jea.DependencyEmitter",
+				// "de.htwg_konstanz.jea.JeaDetector",
+				"de.htwg_konstanz.jea.ProgramBuilder",
+				"de.htwg_konstanz.jea.TccTestClass",
+				"de.htwg_konstanz.jea.vm.ConnectionGraph",
+				"de.htwg_konstanz.jea.vm.DontCareSlot",
+				"de.htwg_konstanz.jea.vm.EscapingTypes",
+				"de.htwg_konstanz.jea.vm.FieldEdge",
+				// "de.htwg_konstanz.jea.vm.Frame",
+				"de.htwg_konstanz.jea.vm.FrameProcessor",
+				"de.htwg_konstanz.jea.vm.Frames",
+				"de.htwg_konstanz.jea.vm.GlobalObject",
+				"de.htwg_konstanz.jea.vm.InternalObject",
+				// "de.htwg_konstanz.jea.vm.LocalVars",
+				"de.htwg_konstanz.jea.vm.MethodSummary", "de.htwg_konstanz.jea.vm.Node",
+				"de.htwg_konstanz.jea.vm.NonObjectNode", "de.htwg_konstanz.jea.vm.ObjectNode",
+				"de.htwg_konstanz.jea.vm.OpStack", "de.htwg_konstanz.jea.vm.Pair",
+				"de.htwg_konstanz.jea.vm.PhantomObject", "de.htwg_konstanz.jea.vm.ReferenceNode",
+				"de.htwg_konstanz.jea.vm.ReturnResult", "de.htwg_konstanz.jea.vm.Slot",
+				"de.htwg_konstanz.jea.vm.SummaryGraph", "de.htwg_konstanz.jea.vm.Triple" };
 
 		Program program = new ProgramBuilder(classes).build();
 		program.print();
 
-		System.out.println("confined classes: " + program.confinedClasses());
+		System.out.println("confined types: ");
+		for (String confinedClass : program.confinedClasses())
+			System.out.println("- " + confinedClass);
+
+		System.out.println("escaping types: ");
+		for (String confinedClass : program.escapingClasses())
+			System.out.println("- " + confinedClass);
+
 	}
 }
