3f8c8c6e1067f76e4225b21784d6e86b7e3b16c2
Oliver Haase <haase@htwg-konstanz.de>
2014-06-04 14:01:27 +0200

    CreationChecking extended from checking if a certain type is created to returning the list of created types.

--- src/main/java/de/htwg_konstanz/jea/CreationChecker.java
+++ src/main/java/de/htwg_konstanz/jea/CreationChecker.java
@@ -1,24 +1,17 @@
 package de.htwg_konstanz.jea;
 
-import java.util.Set;
-
+import de.htwg_konstanz.jea.gen.ByteCodeClass;
 import de.htwg_konstanz.jea.gen.Program;
 
 public class CreationChecker {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		String creator = "de.htwg_konstanz.tcc.TccTestClass";
-		// String creator = "de.htwg_konstanz.tcc.CreationChecker";
-		String creature = "java.lang.String";
-		// String creature = "de.htwg_konstanz.tcc.AstConverter";
+		String creator = "de.htwg_konstanz.jea.vm.DontCareSlot";
 
 		Program program = new ProgramBuilder(creator).build();
 
-		Set<String> methods = program.checkCreation(creature);
-		System.out.println("The following methods of class " + creator + "\n"
-				+ "create instances of class " + creature + ":");
-		for (String method : methods)
-			System.out.println("- " + method);
-
+		for (ByteCodeClass clazz : program.getByteCodeClasss())
+			System.out.println(clazz.getName() + " creates instances of " + clazz.creates());
 	}
+
 }
