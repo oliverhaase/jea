2e27599d9bdbf3311f3c0947f6f5c90aeb9cc292
bestehle <bestehle@htwg-konstanz.de>
2014-10-03 12:56:35 +0200

    activated cacheAll for jastadd

--- src/main/java/de/htwg_konstanz/jea/ConfinementChecker.java
+++ src/main/java/de/htwg_konstanz/jea/ConfinementChecker.java
@@ -5,29 +5,20 @@ import de.htwg_konstanz.jea.gen.Program;
 public class ConfinementChecker {
 
 	public static void main(String[] args) throws ClassNotFoundException {
-		String[] classes = {
-				// "de.htwg_konstanz.jea.AstConverter",
+		String[] classes = { "de.htwg_konstanz.jea.AstConverter",
 				"de.htwg_konstanz.jea.AstConverterVisitor",
-				"de.htwg_konstanz.jea.ConfinementChecker",
-				"de.htwg_konstanz.jea.CreationChecker",
-				// "de.htwg_konstanz.jea.JeaDetector",
-				"de.htwg_konstanz.jea.ProgramBuilder",
-				"de.htwg_konstanz.jea.TccTestClass",
-				"de.htwg_konstanz.jea.vm.Heap",
-				"de.htwg_konstanz.jea.vm.DontCareSlot",
-				"de.htwg_konstanz.jea.vm.EscapingTypes",
-				"de.htwg_konstanz.jea.vm.FieldEdge",
-				// "de.htwg_konstanz.jea.vm.State",
-				"de.htwg_konstanz.jea.vm.StateProcessor",
-				"de.htwg_konstanz.jea.vm.States",
-				"de.htwg_konstanz.jea.vm.GlobalObject",
-				"de.htwg_konstanz.jea.vm.InternalObject",
-				// "de.htwg_konstanz.jea.vm.LocalVars",
-				"de.htwg_konstanz.jea.vm.Node", "de.htwg_konstanz.jea.vm.NonObjectNode",
-				"de.htwg_konstanz.jea.vm.ObjectNode", "de.htwg_konstanz.jea.vm.OpStack",
-				"de.htwg_konstanz.jea.vm.Pair", "de.htwg_konstanz.jea.vm.PhantomObject",
-				"de.htwg_konstanz.jea.vm.ReferenceNode", "de.htwg_konstanz.jea.vm.Slot",
-				"de.htwg_konstanz.jea.vm.Triple" };
+				"de.htwg_konstanz.jea.ConfinementChecker", "de.htwg_konstanz.jea.CreationChecker",
+				"de.htwg_konstanz.jea.JeaDetector", "de.htwg_konstanz.jea.ProgramBuilder",
+				"de.htwg_konstanz.jea.TccTestClass", "de.htwg_konstanz.jea.vm.Heap",
+				"de.htwg_konstanz.jea.vm.DontCareSlot", "de.htwg_konstanz.jea.vm.EscapingTypes",
+				"de.htwg_konstanz.jea.vm.FieldEdge", "de.htwg_konstanz.jea.vm.State",
+				"de.htwg_konstanz.jea.vm.StateProcessor", "de.htwg_konstanz.jea.vm.States",
+				"de.htwg_konstanz.jea.vm.GlobalObject", "de.htwg_konstanz.jea.vm.InternalObject",
+				"de.htwg_konstanz.jea.vm.LocalVars", "de.htwg_konstanz.jea.vm.Node",
+				"de.htwg_konstanz.jea.vm.NonObjectNode", "de.htwg_konstanz.jea.vm.ObjectNode",
+				"de.htwg_konstanz.jea.vm.OpStack", "de.htwg_konstanz.jea.vm.Pair",
+				"de.htwg_konstanz.jea.vm.PhantomObject", "de.htwg_konstanz.jea.vm.ReferenceNode",
+				"de.htwg_konstanz.jea.vm.Slot", "de.htwg_konstanz.jea.vm.Triple" };
 
 		Program program = new ProgramBuilder(classes).build();
 		program.print();
