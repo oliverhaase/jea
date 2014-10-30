4bdc87abd59410c4df84689c5eea836cc31099ac
StefanW <stefan.waldraff@gmail.com>
2014-10-30 15:50:42 +0100

    fixed typecast exceptions and added some playground methods for testing

--- src/main/java/de/htwg_konstanz/jea/annotation/AnnotationHelper.java
+++ src/main/java/de/htwg_konstanz/jea/annotation/AnnotationHelper.java
@@ -10,6 +10,7 @@ import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import javassist.CtNewMethod;
 import javassist.NotFoundException;
 import javassist.bytecode.AnnotationsAttribute;
 import javassist.bytecode.ConstPool;
@@ -18,6 +19,13 @@ import javassist.bytecode.annotation.AnnotationMemberValue;
 import javassist.bytecode.annotation.ArrayMemberValue;
 import javassist.bytecode.annotation.MemberValue;
 import javassist.bytecode.annotation.StringMemberValue;
+
+import com.google.common.base.Objects;
+
+import de.htwg_konstanz.jea.ProgramBuilder;
+import de.htwg_konstanz.jea.gen.ByteCodeClass;
+import de.htwg_konstanz.jea.gen.Method;
+import de.htwg_konstanz.jea.gen.Program;
 import de.htwg_konstanz.jea.vm.Heap;
 
 public final class AnnotationHelper {
@@ -26,8 +34,8 @@ public final class AnnotationHelper {
 		throw new AssertionError("May not be called");
 	}
 
-	public static boolean writeAnnotationToMethod(CtClass clazz,
-			CtMethod method, Annotation annotation, ConstPool cp) {
+	public static boolean writeAnnotationToMethod(CtClass clazz, CtMethod method,
+			Annotation annotation, ConstPool cp) {
 		AnnotationsAttribute attribute = new AnnotationsAttribute(cp,
 				AnnotationsAttribute.visibleTag);
 		attribute.addAnnotation(annotation);
@@ -39,7 +47,7 @@ public final class AnnotationHelper {
 			success = false;
 			e.printStackTrace();
 		}
-		;
+		
 		return success;
 	}
 
@@ -53,20 +61,28 @@ public final class AnnotationHelper {
 		return annotation;
 	}
 
-	public static ArrayMemberValue convertToAnnotationArray(
-			List<MemberValue> list, ConstPool cp) {
-		ArrayMemberValue value = new ArrayMemberValue(
-				new AnnotationMemberValue(cp), cp);
-		value.setValue(list.toArray(new AnnotationMemberValue[list.size()]));
+	public static ArrayMemberValue convertToAnnotationArray(List<MemberValue> list, ConstPool cp) {
+		ArrayMemberValue value = new ArrayMemberValue(new AnnotationMemberValue(cp), cp);
+		value.setValue(list.toArray(new MemberValue[list.size()]));
 		return value;
 	}
 
-	public static ArrayMemberValue convertToStringArray(List<MemberValue> list,
-			ConstPool cp) {
-		ArrayMemberValue value = new ArrayMemberValue(
-				new StringMemberValue(cp), cp);
-		value.setValue(list.toArray(new AnnotationMemberValue[list.size()]));
+	public static ArrayMemberValue convertToStringArray(List<MemberValue> list, ConstPool cp) {
+		ArrayMemberValue value = new ArrayMemberValue(new StringMemberValue(cp), cp);
+		value.setValue(list.toArray(new MemberValue[list.size()]));
 		return value;
+	}
+
+	static Heap getMethodSummaryFromClassWithOneMethod(String path) throws ClassNotFoundException {
+		Program program = new ProgramBuilder(path).build();
+
+		for (ByteCodeClass clazz : program.getByteCodeClassList()) {
+			for (Method method : clazz.getMethods()) {
+				return method.methodSummary().doFinalStuff();
+			}
+		}
+
+		return null;
 	}
 
 	/**
@@ -78,8 +94,8 @@ public final class AnnotationHelper {
 	 * @throws NotFoundException
 	 * @throws ClassNotFoundException
 	 */
-	static void write() throws FileNotFoundException, IOException,
-			RuntimeException, NotFoundException, ClassNotFoundException {
+	static void write() throws FileNotFoundException, IOException, RuntimeException,
+			NotFoundException, ClassNotFoundException {
 		ClassPool pool = ClassPool.getDefault();
 		// there is something wrong with the paths!!
 		CtClass clazz = pool.makeClass(new FileInputStream(
@@ -89,8 +105,7 @@ public final class AnnotationHelper {
 
 		Heap heap = new Heap();
 		Annotation annotation = heap.convertToAnnotation(cp);
-		System.out.println(AnnotationHelper.writeAnnotationToMethod(clazz,
-				method, annotation, cp));
+		System.out.println(AnnotationHelper.writeAnnotationToMethod(clazz, method, annotation, cp));
 	}
 
 	/**
@@ -102,15 +117,32 @@ public final class AnnotationHelper {
 	 * @throws NotFoundException
 	 * @throws ClassNotFoundException
 	 */
-	static void read() throws FileNotFoundException, IOException,
-			RuntimeException, NotFoundException, ClassNotFoundException {
+	static void read() throws FileNotFoundException, IOException, RuntimeException,
+			NotFoundException, ClassNotFoundException {
 		ClassPool pool = ClassPool.getDefault();
-		CtClass clazz = pool.makeClass(new FileInputStream(
-				"playground\\MyTestClass.class"));
+		CtClass clazz = pool.makeClass(new FileInputStream("playground\\MyTestClass.class"));
 		CtMethod method = clazz.getDeclaredMethod("foo");
 		MethodSummaryAnnotation anno = (MethodSummaryAnnotation) method
 				.getAnnotation(MethodSummaryAnnotation.class);
 		System.out.println(Heap.newInstanceByAnnotation(anno).toString());
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, CannotCompileException {
+		String classPath = "de\\htwg_konstanz\\jea\\AstConverter";
+		Heap methodSummary = getMethodSummaryFromClassWithOneMethod(classPath);
+
+		ClassPool pool = ClassPool.getDefault();
+		CtClass testClass = pool.makeClass("TestClass");
+		ConstPool cp = testClass.getClassFile().getConstPool();
+		CtMethod method = CtNewMethod.make("public int ymove(int dy) { return 0; }", testClass);
+		testClass.addMethod(method);
+
+		Annotation annotation = methodSummary.convertToAnnotation(cp);
+		writeAnnotationToMethod(testClass, method, annotation, cp);
+
+		MethodSummaryAnnotation fromFile = (MethodSummaryAnnotation) method
+				.getAnnotation(MethodSummaryAnnotation.class);
+		System.out.println(Objects.equal(methodSummary, Heap.newInstanceByAnnotation(fromFile)));
 	}
 
 }
