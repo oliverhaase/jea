952b646d77958c7f7f37e36f4c6178b5771b9617
bestehle <bestehle@htwg-konstanz.de>
2014-11-20 20:58:11 +0100

    use all jars in ClassPath and enabled subTypes for java.lang.Objects

--- src/main/java/de/htwg_konstanz/jea/ClassPathFinder.java
+++ src/main/java/de/htwg_konstanz/jea/ClassPathFinder.java
@@ -6,16 +6,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import org.reflections.Reflections;
-import org.reflections.scanners.ResourcesScanner;
 import org.reflections.scanners.SubTypesScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
-import org.reflections.util.FilterBuilder;
 
 public class ClassPathFinder {
 
@@ -25,11 +22,12 @@ public class ClassPathFinder {
 	private Reflections reflections;
 
 	private ClassPathFinder() {
-		urls = ClasspathHelper.forClassLoader();// JavaClassPath();
+		urls = ClasspathHelper.forClassLoader();
 
 		addPlattformClasses();
 
-		reflections = new Reflections(new ConfigurationBuilder().setUrls(urls));
+		reflections = new Reflections(new ConfigurationBuilder().setScanners(
+				new SubTypesScanner(false)).setUrls(urls));
 	}
 
 	public static ClassPathFinder getInstance() {
@@ -41,12 +39,7 @@ public class ClassPathFinder {
 
 	private void addPlattformClasses() {
 		File file = new File(System.getProperty("java.home").replaceAll("\\\\", "/") + "/lib/");
-		// List<String> jars = getJarsInDir(file);
-		String[] jars = { "C:\\Program Files\\Java\\jre7\\lib\\charsets.jar",
-				"C:\\Program Files\\Java\\jre7\\lib\\jce.jar",
-				"C:\\Program Files\\Java\\jre7\\lib\\jfr.jar",
-				"C:\\Program Files\\Java\\jre7\\lib\\resources.jar",
-				"C:\\Program Files\\Java\\jre7\\lib\\rt.jar" };
+		List<String> jars = getJarsInDir(file);
 		try {
 			for (String string : jars) {
 				urls.add(new URL("file:\\\\\\" + string));
@@ -61,22 +54,34 @@ public class ClassPathFinder {
 	 * itself.
 	 */
 	public Set<String> getSubTypsOf(String type) {
-		Set<?> types = null;
+		Set<String> types = reflections.getStore().getSubTypesOf(type); // getSubTypesOf(Class.forName(type));
+		types.add(type);
+		return types;
+	}
+
+	public static String[] getClassesByPath(String packageName) {
+	
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+	
+		String path = packageName.replace('.', '/');
+	
+		Enumeration<URL> resources = null;
 		try {
-			types = reflections.getSubTypesOf(Class.forName(type));
-		} catch (ClassNotFoundException e) {
+			resources = classLoader.getResources(path);
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
-		HashSet<String> classes = new HashSet<String>();
-		classes.add(type);
-
-		for (Object object : types) {
-			Class<?> c = (Class<?>) object;
-			classes.add(c.getName());
+	
+		List<File> dirs = new ArrayList<File>();
+	
+		while (resources.hasMoreElements())
+			dirs.add(new File(resources.nextElement().getFile()));
+	
+		ArrayList<String> classes = new ArrayList<String>();
+		for (File directory : dirs) {
+			classes.addAll(findClasses(directory, packageName));
 		}
-
-		return classes;
+		return classes.toArray(new String[0]);
 	}
 
 	private static List<String> findClasses(File directory, String packageName) {
@@ -96,31 +101,6 @@ public class ClassPathFinder {
 		return classes;
 	}
 
-	public static String[] getClassesByPath(String packageName) {
-
-		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-
-		String path = packageName.replace('.', '/');
-
-		Enumeration<URL> resources = null;
-		try {
-			resources = classLoader.getResources(path);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		List<File> dirs = new ArrayList<File>();
-
-		while (resources.hasMoreElements())
-			dirs.add(new File(resources.nextElement().getFile()));
-
-		ArrayList<String> classes = new ArrayList<String>();
-		for (File directory : dirs) {
-			classes.addAll(findClasses(directory, packageName));
-		}
-		return classes.toArray(new String[0]);
-	}
-
 	private static List<String> getJarsInDir(File directory) {
 		List<String> jars = new ArrayList<String>();
 
@@ -137,25 +117,4 @@ public class ClassPathFinder {
 		}
 		return jars;
 	}
-
-	public static String[] getClassesByReflection(String packageName) {
-		ClassLoader[] classLoaders = { ClasspathHelper.contextClassLoader(),
-				ClasspathHelper.staticClassLoader() };
-
-		Reflections reflections = new Reflections(new ConfigurationBuilder()
-				.setScanners(new SubTypesScanner(false), new ResourcesScanner())
-				.setUrls(ClasspathHelper.forClassLoader(classLoaders))
-				.filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(packageName))));
-
-		Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
-
-		int i = 0;
-		String[] classNames = new String[classes.size()];
-		for (Class<?> cls : classes) {
-			classNames[i++] = cls.getName();
-		}
-
-		return classNames;
-	}
-
 }
